47a9ab95adbdff5bc7c851cedcda40e10b16005a
stevetsim <jim97731@gmail.com>
2020-10-31 17:24:37 +0800

    Project Init

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+// Vue Core
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
+// Import libraries
+import VueMeta from 'vue-meta'
+
+Vue.config.productionTip = false
+Vue.use(VueMeta)
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
